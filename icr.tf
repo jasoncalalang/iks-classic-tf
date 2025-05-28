aa1d73d1bd120c0f583f6c4f3785bf8d12230121
Jason Calalang <jcalalang@ph.ibm.com>
2025-05-28 21:03:52 +0800

    remove block

--- icr.tf
+++ icr.tf
@@ -7,15 +7,6 @@ variable "icr_instance_name" {
   default = "da-nonprod"
 }
 
-resource "ibm_resource_instance" "icr_nonprod" {
-  name              = var.icr_instance_name
-  service           = "container-registry"
-  plan              = "standard"
-  location          = var.region
-  resource_group_id = data.ibm_resource_group.target.id
-  tags              = ["nonprod", "icr", "terraform"]
-}
-
 resource "ibm_cr_namespace" "icr_namespace" {
   name               = var.icr_instance_name 
   resource_group_id  = data.ibm_resource_group.target.id
