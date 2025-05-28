91129ac19654bd4f2aed3e03a026d30c1fd43fe3
Jason Calalang <jcalalang@ph.ibm.com>
2025-05-28 15:45:33 +0800

    add container registry

---
+++ icr.tf
@@ -0,0 +1,17 @@
+###############################################################################
+# IBM Container Registry – non-prod
+###############################################################################
+
+variable "icr_instance_name" {
+  type = string
+  default = "da-nonprod"
+}
+
+resource "ibm_resource_instance" "icr_nonprod" {
+  name              = var.icr_instance_name
+  service           = "container-registry"
+  plan              = "standard"
+  location          = var.region
+  resource_group_id = data.ibm_resource_group.target.id
+  tags              = ["nonprod", "icr", "terraform"]
+}
